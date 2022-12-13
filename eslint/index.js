4acd9d70403206f44a9fb01e57e7dc8e5cbe6113
Roger Schönbächler <roger@authentiq.ch>
2022-12-13 17:34:31 +0100

    refactor: group internal modules separately

--- eslint/index.js
+++ eslint/index.js
@@ -59,8 +59,35 @@ module.exports = {
     "import/no-duplicates": "error",
     "simple-import-sort/exports": "warn",
     "sort-imports": "off",
-    "simple-import-sort/imports": "warn",
-
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        "groups": [
+          // Node.js builtins. You could also generate this regex if you use a `.js` config.
+          // For example: `^(${require("module").builtinModules.join("|")})(/|$)`
+          // Note that if you use the `node:` prefix for Node.js builtins,
+          // you can avoid this complexity: You can simply use "^node:".
+          [
+            "^(assert|buffer|child_process|cluster|console|constants|crypto|dgram|dns|domain|events|fs|http|https|module|net|os|path|punycode|querystring|readline|repl|stream|string_decoder|sys|timers|tls|tty|url|util|vm|zlib|freelist|v8|process|async_hooks|http2|perf_hooks)(/.*|$)"
+          ],
+          // External packages.
+          ["^"],
+          // Internal packages.
+          ["^(@)(/.*|$)"],
+          ["^(~)(/.*|$)"],
+          // Side effect imports.
+          ["^\\u0000"],
+          // Parent imports. Put `..` last.
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          // Other relative imports. Put same-folder imports and `.` last.
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+          // Style imports.
+          ["^.+\\.s?css$"],
+          // JSON imports.
+          ["^.+\\.jsonc?$"]
+        ]
+      }
+    ],
 
     // Sort object keys
 
