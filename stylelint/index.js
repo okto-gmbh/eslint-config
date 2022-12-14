e737156597f0117bdb6c7a87b28216df849fe9b9
Roger Schönbächler <roger@authentiq.ch>
2022-12-14 16:36:40 +0100

    feat: add stylelint config

---
+++ stylelint/index.js
@@ -0,0 +1,27 @@
+module.exports = {
+  customSyntax: "@stylelint/postcss-css-in-js",
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-recommended",
+    "stylelint-config-idiomatic-order",
+    "stylelint-config-prettier",
+  ],
+  rules: {
+    "custom-property-empty-line-before": null,
+    "declaration-empty-line-before": null,
+    "function-name-case": null,
+    "function-no-unknown": [true, { ignoreFunctions: ["/\\${/"] }],
+    "no-descending-specificity": null,
+    "no-duplicate-selectors": null,
+    "no-empty-source": null,
+    "no-invalid-double-slash-comments": null,
+    "no-missing-end-of-source-newline": null,
+    "property-no-unknown": [
+      true,
+      {
+        ignoreProperties: ["content-visibility"],
+      },
+    ],
+    "value-keyword-case": null,
+  },
+};
