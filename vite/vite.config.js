347f00dfee88f37a62461f90ab5688a120fb7884
Roger Schönbächler <rs@okto.ch>
2023-05-16 17:34:15 +0200

    fix(vite): config export

--- vite/vite.config.js
+++ vite/vite.config.js
@@ -1,14 +1,18 @@
-const tsconfigPaths = require('vite-tsconfig-paths')
+const { default: tsconfigPaths } = require('vite-tsconfig-paths')
 const GithubActionsReporter = require('vitest-github-actions-reporter')
+const defineConfig = require('vitest/config').defineConfig
 
-export default {
-    plugins: [tsconfigPaths()],
-    test: {
-        include: [
-            '**/*.{test,test-d,spec,spec-d}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
-        ],
-        reporters: process.env.GITHUB_ACTIONS
-            ? ['verbose', new GithubActionsReporter()]
-            : 'default'
-    }
+module.exports = {
+    config: {
+        plugins: [tsconfigPaths()],
+        test: {
+            include: [
+                '**/*.{test,test-d,spec,spec-d}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'
+            ],
+            reporters: process.env.GITHUB_ACTIONS
+                ? ['verbose', new GithubActionsReporter()]
+                : 'default'
+        }
+    },
+    defineConfig
 }
