adf75aba18e6ed275b8b98f8698c6b9a1bca1d5b
Roger Schönbächler <rs@okto.ch>
2023-06-23 08:29:38 +0200

    feat: warn on floating promises

--- eslint/index.js
+++ eslint/index.js
@@ -200,10 +200,15 @@ module.exports = {
                     }
                 ],
                 'typescript-sort-keys/string-enum': 'warn',
+
+                // Type imports
                 '@typescript-eslint/consistent-type-imports': [
                     'warn',
                     { disallowTypeAnnotations: false }
-                ]
+                ],
+
+                // Promises
+                '@typescript-eslint/no-floating-promises': 'warn'
             }
         },
 
